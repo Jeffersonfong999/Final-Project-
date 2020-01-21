f66c66e7bb022aae5c8476365ec83f0c371aa73e
Jeffersonfong999 <55213475+Jeffersonfong999@users.noreply.github.com>
2020-01-21 15:57:52 -0700

    Delete MathExtention.js

--- Final-Project--master/FINAL PROJECT/MathExtention.js
+++
@@ -1,8 +0,0 @@
-//Random Intergers value for nicknameIndex
-Math.randomDec = function (low,high){
-    return Math.random() * (high - low) + low 
-}
-
-Math.randomInt = function (low, high){
-    return Math.floor(Math.randomDec(low, high))  
-}
